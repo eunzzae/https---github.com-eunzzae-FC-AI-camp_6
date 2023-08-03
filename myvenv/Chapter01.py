ce97d8185b1d85263b652aa21c6dc38792100caf
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-08-03 15:10:22 +0900

    리스트컴프리헨션 예제1

---
+++ myvenv/Chapter01.py
@@ -0,0 +1,14 @@
+#1. 아래 딕셔너리에서, 모든 학생들의 영어 점수 총합을 출력하는 코드를 작성하세요.
+# scores = {
+#   'Kim': {'math': 50, 'english': 70},
+#   'Park': {'math': 70, 'english': 60},
+#   'Lee': {'math': 80, 'english': 50}
+# }
+
+# total=0
+# for score in scores.values():
+#     total += score['english']
+# print(total)
+
+# total=sum([score['english'] for score in scores.values()])
+# print(total)
