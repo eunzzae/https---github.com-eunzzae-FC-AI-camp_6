a9e48c8f31c5163eb3ea0c10642c4fe2b8531f1a
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-08-04 16:56:32 +0900

    음성인식 기능 생성

---
+++ myvenv/230804/Chapter06.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+# 인식을 위한 객체 생성
+r = sr.Recognizer()
+
+# 마이크 사용을 위한 객체 생성
+mic = sr.Microphone()
+with mic as source: # 마이크에 담긴 소리를 토대로 아래 코드 실행
+    print('인식 중...')
+    audio = r.listen(source, timeout=5, phrase_time_limit=5) # 해당 소리를 오디오 파일 형태로 변환
+
+try:
+    result = r.recognize_google(audio, language = "ko-KR") # 오디오를 토대로 음성 인식
+    print('결과: ' + result) # 인식 결과 출력
+except sr.UnknownValueError:
+    print("음성 인식 실패")
+except sr.RequestError:
+    print("서버 에러 발생")
+except sr.WaitTimeoutError:
+    print("인식 실패")
