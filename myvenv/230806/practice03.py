48e1d76ba7051ed029bf35660dd76182443c3cf8
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-08-06 14:53:18 +0900

    날씨 api

--- myvenv/230806/practice03.py
+++ myvenv/230806/practice03.py
@@ -1,3 +1,6 @@
+# 웹 크롤링 
+# 멜론 차트 100위까지 순위가져오기
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
@@ -6,7 +9,8 @@ import csv
 def openDriver():
     url = 'https://www.melon.com/chart/index.htm'
     driver = webdriver.Chrome()
-    driver.implicitly_wait(3)
+    # time_sleep() 과 비슷하지만 웹크롤링 할 때는 이 함수를 사용하는 것이 좋음
+    driver.implicitly_wait(3) 
     driver.get(url)
     time.sleep(1)
     return driver
